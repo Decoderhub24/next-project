6f40f0d686c0b45b94471e5bee2d4f58832079bc
Decoderhub24 <adhikaripralhad79@gmail.com>
2025-01-18 11:44:46 +0545

    second commit

--- app/components/erpcontent.js
+++ app/components/erpcontent.js
@@ -1,6 +1,6 @@
 // In your ERPContent.js (or ERPContent.jsx)
 import styles from '../styles/ERPcontent.module.css';  
-import erpImage from '../images/bg1.png';
+
 
 
 export default function ERPContent() {
@@ -8,11 +8,17 @@ export default function ERPContent() {
     <div className={styles['erp-content-container']}>
       <div className={styles['erp-content']}>
         <div className={styles['erp-image']}>
-          <img src="./images/bg1.png" alt="ERP System" className={styles['image']} />
+          <img src="https://hitechnepal.com/images/erp-infograph.png" alt="ERP System" className={styles['image']} />
         </div>
         <div className={styles['erp-text']}>
-          <h2>ERP System</h2>
-          <p>ERP systems integrate core business processes into one unified system.</p>
+          <h2>ERP </h2>
+          <p>Enterprise resource planning (ERP) is an integrated management of core business processes, often in real time and mediated by software and technology.
+
+Hitech IMS is a modular Cloud based ERP software with multi-branch and complete Enterprise Solution management.
+
+Track and manage your business through our ERP solution where you can easily manage your raw materials, productions, administration, finances and accounts, purchases and imports, sales and budget, customer care and reporting.
+
+</p>
           <button className={styles['read-more-btn']}>Read More</button>
         </div>
       </div>
